2e69f78f2ddb34423a8b98e38da03128ebd7f0e6
Mick Arias <mick756@gmail.com>
2019-08-20 10:37:36 -0700

    port change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require("express");
 let path = require("path");
 
 let app = express();
-let PORT = 3000;
+let PORT = 80;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
