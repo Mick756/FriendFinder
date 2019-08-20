4e28962081ccab72a9ae52699a0bdeb2959accdf
Mick Arias <mick756@gmail.com>
2019-08-20 10:24:04 -0700

    Added server functionality.

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 let express = require("express");
+let path = require("path");
+
+let app = express();
+let PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app", "public", "home.html"));
+});
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app", "public", "survey.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
